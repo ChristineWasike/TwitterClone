ed960a702f11c6b26200d69a27b4c60072bdfbfd
Christine Wasike <christinewasike22@gmail.com>
2018-01-07 20:18:49 +0300

    This is the main activity, yet to figure out what to do with it, most probably it will the chat rooms for the twitter users

--- app/src/main/java/com/wasike/twitterclone/ui/MainActivity.java
+++ app/src/main/java/com/wasike/twitterclone/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.wasike.twitterclone;
+package com.wasike.twitterclone.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wasike.twitterclone.R;
 
 public class MainActivity extends AppCompatActivity {
 
